1e665db2217bcef8bcc3ce6630671ddc9eebdf43
ricardocodem <52762615+ricardocodem@users.noreply.github.com>
2021-09-16 20:47:36 -0300

    Initial commit Notifier

---
+++ part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
@@ -0,0 +1,36 @@
+package notifier;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class NotifierApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(NotifierApplication.class);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        TextField text = new TextField();
+        Button button = new Button("Button");
+        Label label = new Label();
+
+        button.setOnAction((event) -> {
+            label.setText(text.getText());
+        });
+
+        VBox group = new VBox();
+        group.getChildren().addAll(text, button, label);
+
+        Scene scene = new Scene(group);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
