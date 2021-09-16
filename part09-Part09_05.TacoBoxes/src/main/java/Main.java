4ea2ebb7d24c1f9d0e652666675a1ecb21ef30b1
ricardocodem <52762615+ricardocodem@users.noreply.github.com>
2021-09-16 19:48:54 -0300

    Initial commit TacoBoxes

---
+++ part09-Part09_05.TacoBoxes/src/main/java/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Test your code here!
+        TripleTacoBox taco = new TripleTacoBox();
+        System.out.println(taco.tacosRemaining());
+        taco.eat();
+        System.out.println(taco.tacosRemaining());
+    }
+}
