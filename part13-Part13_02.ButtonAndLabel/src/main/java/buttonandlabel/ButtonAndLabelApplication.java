57816391619b0cbf452375bf5a3e6bf2b4b8189e
ricardocodem <52762615+ricardocodem@users.noreply.github.com>
2021-09-16 20:44:42 -0300

    Initial commit ButtonAndLabel

---
+++ part13-Part13_02.ButtonAndLabel/src/main/java/buttonandlabel/ButtonAndLabelApplication.java
@@ -0,0 +1,31 @@
+package buttonandlabel;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class ButtonAndLabelApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(ButtonAndLabelApplication.class);
+    }
+
+    @Override
+    public void start(Stage window) throws Exception {
+        Label textComponent = new Label("This is a label");
+        Button buttonComponent = new Button("This is a button");
+
+        FlowPane componentGroup = new FlowPane();
+        componentGroup.getChildren().add(textComponent);
+        componentGroup.getChildren().add(buttonComponent);
+
+        Scene view = new Scene(componentGroup);
+
+        window.setScene(view);
+        window.show();
+    }
+
+}
