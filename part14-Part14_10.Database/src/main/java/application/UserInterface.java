7975a8435d7863182c6d8359d90ba7971abf4140
ricardocodem <52762615+ricardocodem@users.noreply.github.com>
2021-09-16 21:00:25 -0300

    Initial commit Database

---
+++ part14-Part14_10.Database/src/main/java/application/UserInterface.java
@@ -0,0 +1,60 @@
+package application;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class UserInterface {
+
+    private Scanner scanner;
+    private TodoDao database;
+
+    public UserInterface(Scanner scanner, TodoDao database) {
+        this.scanner = scanner;
+        this.database = database;
+    }
+
+    public void start() throws SQLException {
+        while (true) {
+            System.out.println("");
+            System.out.println("Enter command:");
+            System.out.println("1) list");
+            System.out.println("2) add");
+            System.out.println("3) mark as done");
+            System.out.println("4) remove");
+            System.out.println("x) quit");
+
+            System.out.print("> ");
+            String command = this.scanner.nextLine();
+            if (command.equals("x")) {
+                break;
+            }
+
+            // implement the functionality here
+            if (command.equals("1")) {
+                System.out.println("Listing the database contents");
+                System.out.println(database.list());
+            }
+            if (command.equals("2")) {
+                System.out.println("Adding a new todo");
+                System.out.println("Enter name");
+                String todoName = scanner.nextLine();
+                System.out.println("Enter description");
+                String todoDescription = scanner.nextLine();
+                database.add(new Todo(todoName, todoDescription, Boolean.FALSE));
+            }
+            if (command.equals("3")) {
+                System.out.println("Which todo should be marked as done (give the id)?");
+                int id = Integer.valueOf(scanner.nextLine());
+                database.markAsDone(id);
+            }
+            if (command.equals("4")) {
+                System.out.println("Which todo should be removed (give the id)?");
+                int id = Integer.valueOf(scanner.nextLine());
+                database.remove(id);
+            }
+        }
+
+        System.out.println("Thank you!");
+    }
+
+}
